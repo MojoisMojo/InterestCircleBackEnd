f90dfac6c7bda097ab67b9a0c572adffec68297d
MojoisMe <2377862945@qq.com>
2024-08-04 14:28:03 +0800

    add a little test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.[jt]s?(x)', '**/test/**/*.spec.[jt]s?(x)'],
   testPathIgnorePatterns: ['<rootDir>/test/fixtures'],
   coveragePathIgnorePatterns: ['<rootDir>/test/'],
 };
